3298ac2b6ac518f362ac2ad16827f479ace063e5
Aurelien <linux.aurelien@gmail.com>
2023-01-23 11:32:20 +0100

    preparation template du composant

--- projects/password-generator/src/app/app.component.ts
+++ projects/password-generator/src/app/app.component.ts
@@ -2,9 +2,45 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="container">
+      <h1>Générez un mot de passe fort !</h1>
+      <div class="grid">
+        <div>
+          <h3>Votre futur mot de passe :</h3>
+          <article>{{ message }}</article>
+        </div>
+        <div>
+          <label for="length">Longueur du mot de passe :</label>
+          <input id="length" type="range" min="10" max="50" name="length" />
+
+          <label>
+            <input
+              role="switch"
+              type="checkbox"
+              name="uppercase"
+              id="uppercase"
+            />
+            Contiendra des majuscules
+          </label>
+
+          <label>
+            <input role="switch" type="checkbox" name="numbers" id="numbers" />
+            Contiendra des nombres
+          </label>
+
+          <label>
+            <input role="switch" type="checkbox" name="symbols" id="symbols" />
+            Contiendra des caractères spéciaux
+          </label>
+          <hr />
+          <button>Générer</button>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: [],
 })
 export class AppComponent {
-  title = 'password-generator';
+  message = 'Cliquez sur le bouton "Générer"';
 }
