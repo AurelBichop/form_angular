9d47979ef6542689959f80e01a9b94615d5423ff
Aurelien <linux.aurelien@gmail.com>
2023-01-30 16:11:37 +0100

    modification avec bouton exemple

---
+++ projects/playground/src/app/directives-attributs/model.directive.ts
@@ -0,0 +1,30 @@
+import {
+  Directive,
+  EventEmitter,
+  HostListener,
+  Output,
+  Input,
+  HostBinding,
+} from '@angular/core';
+
+@Directive({
+  selector: '[model]',
+})
+export class ModelDirective {
+  @Output('modelChange')
+  onValueChange = new EventEmitter();
+
+  @Input('model')
+  @HostBinding('value')
+  value: any;
+
+  @HostListener('input', ['$event.target'])
+  onInput(element: HTMLInputElement) {
+    if (element.type === 'number') {
+      this.onValueChange.emit(element.valueAsNumber);
+      return;
+    }
+
+    this.onValueChange.emit(element.value);
+  }
+}
