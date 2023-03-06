a2d22cf1fe4c0c2933b880016da61a62f22e2b10
Aurelien <linux.aurelien@gmail.com>
2023-03-06 10:57:52 +0100

    exemple avec la micro syntax et le else

--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -9,18 +9,20 @@ import {
   selector: 'app-root',
   template: `<h1>Découverte Angular</h1>
 
-    <h2 *if="age > 18; else other">Le If avec etoile fonctionne</h2>
-
-    <ng-template #monTemplate [if]="age > 18" [ifElse]="autre">
-      <h2>Le IF Fonctionne</h2>
-    </ng-template>
-
-    <ng-template #other>
-      <h2>Le ELSE avec If* Fonctionne</h2>
-    </ng-template>
+    <div *if="age >= 18; else autre">
+      <h2>Vous êtes Majeur !</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+        repellendus delectus quod, natus sapiente aliquid consequuntur,
+        voluptatem deserunt tempore quaerat laboriosam enim ea incidunt,
+        provident id explicabo vitae eum dolorem.
+      </p>
+      <button>Voir le site</button>
+    </div>
 
     <ng-template #autre>
-      <h2>Le ELSE Fonctionne</h2>
+      <h2>Vous n'êtes pas Majeur</h2>
+      <p>Merci de sortir du Site</p>
     </ng-template>
 
     <input type="number" placeholder="Votre age" #ageInput />
