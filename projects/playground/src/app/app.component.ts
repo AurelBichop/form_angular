ebe3513233aaf64f7360b8704f839f60540ab520
Aurelien <linux.aurelien@gmail.com>
2023-03-06 09:55:53 +0100

    Projeter un template

--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -1,40 +1,39 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `<h1>Découverte Angular</h1>
-    <newsletter
-      (confirm)="onConfirm($event)"
-      title="Recevez nos courriers"
-      button-text="confirmer"
-      placeholder="votre adresse mail svp"
-    >
-      <h2>Hello H2</h2>
-      <p>Vous recevrez l'ensemble de nos informations</p>
-      <a href="">en savoir plus</a>
-    </newsletter>
+    <ng-template #monTemplate>
+      <h2>Hello-World</h2>
+    </ng-template>
 
-    <card title="Ma carte">
-      <p>Lorem Ipsum dolor sit ..</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
-        nesciunt ipsum minus voluptas, repudiandae debitis!
-      </p>
-      <footer><a href="#">en savoir plus</a></footer>
-    </card> `,
-  styles: [
-    `
-      .red {
-        color: red;
-      }
-      .bold {
-        font-weight: bold;
-      }
-    `,
-  ],
+    <ng-container #monContainer>
+      <h3>Je suis le Container</h3>
+    </ng-container>
+
+    <button (click)="onClick()">Tester</button> `,
+  styles: [``],
 })
 export class AppComponent {
-  onConfirm(email: string) {
-    console.log("Boutton cliqué depuis l'extérieur :", email);
+  @ViewChild('monTemplate')
+  monTemplate?: TemplateRef<any>;
+
+  @ViewChild('monContainer', { read: ViewContainerRef })
+  monContainer?: ViewContainerRef;
+
+  onClick() {
+    if (this.monTemplate) {
+      this.monContainer?.clear();
+      this.monContainer?.createEmbeddedView(this.monTemplate);
+    }
+  }
+
+  ngAfterViewInit() {
+    console.log(this.monContainer, this.monTemplate);
   }
 }
