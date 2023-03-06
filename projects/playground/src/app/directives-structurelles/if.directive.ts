da435fdb18e1eed9a687b59802562f7a14dfcfff
Aurelien <linux.aurelien@gmail.com>
2023-03-06 11:11:37 +0100

    exemple avec les div

---
+++ projects/playground/src/app/directives-structurelles/if.directive.ts
@@ -0,0 +1,36 @@
+import {
+  Directive,
+  ElementRef,
+  Input,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
+
+@Directive({
+  selector: '[if]',
+})
+export class IfDirective {
+  @Input('if')
+  condition: boolean = false;
+
+  @Input('ifElse')
+  autreTemplateRef?: TemplateRef<any>;
+
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private containerRef: ViewContainerRef
+  ) {}
+
+  ngOnChanges() {
+    this.containerRef.clear();
+
+    if (this.condition) {
+      this.containerRef.createEmbeddedView(this.templateRef);
+      return;
+    }
+
+    if (this.autreTemplateRef) {
+      this.containerRef.createEmbeddedView(this.autreTemplateRef);
+    }
+  }
+}
